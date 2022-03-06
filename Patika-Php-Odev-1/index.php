25852b448cae18eb6513b73cfe87fb42bb4bdb96
M.Burak Kotan <67208320+mbktn@users.noreply.github.com>
2022-03-06 19:26:47 +0300

    Add files via upload

---
+++ Patika-Php-Odev-1/index.php
@@ -0,0 +1,53 @@
+
+
+
+
+
+<?php
+
+
+function value($tekrar){
+
+$sayac=0;
+    for ($i=0;$i<$tekrar;$i++){
+
+        while ($sayac<=$i){
+
+            echo '0';
+            $sayac++;
+        }
+
+        echo '<br>';
+        $sayac=0;
+    }
+};
+
+value(15);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
